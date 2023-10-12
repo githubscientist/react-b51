cd28eb19056783396adc929c17fec7d509c4089f
githubscientist <sathish.datascientist@gmail.com>
2023-10-12 20:31:32 +0530

    git commands added --in App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -5,3 +5,25 @@ const App = () => {
 }
 
 export default App;
+
+/*
+  - intialize git repo:
+
+    git init
+
+  - status of the changes:
+
+    git status
+
+  - show the logs of all the commits:
+
+    git log
+
+  - create a new branch
+
+    git branch branchname
+
+  - switch to a branch or commit
+
+    git checkout branchname/commit ID
+*/
