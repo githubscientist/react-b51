4e329af98fa18cd9914b60e548627fe37bcb740a
githubscientist <sathish.datascientist@gmail.com>
2023-10-13 19:12:44 +0530

    component as an arrow function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
-// create a new component called App
-// a component is a piece of UI or group of multiple UI pieces
-// a component can be created as a javascript function
-// it returns HTML with javascript embedded
-function App() {
-  return <p>Hello World!</p>
+const App = () => {
+  return <p>Hello World</p>
 }
 
 export default App;
