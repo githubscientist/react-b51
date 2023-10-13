5280901be398c2dbf9d09ecd00f4f5fbb933edc5
githubscientist <sathish.datascientist@gmail.com>
2023-10-13 20:04:38 +0530

    conditional rendering --way3

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Note({ note }) {
 
   return (
-    <li>{ note.important ? note.content + ' ✯' : note.content }</li>  
+      <li>{note.content} { note.important && '✯' }</li>  
   )
 }
 
