5f05486b7ba56ba9aa811123242e4568be2f56bb
githubscientist <sathish.datascientist@gmail.com>
2023-10-13 19:50:00 +0530

    rendering array lists

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,11 @@ function App({notes}) {
     <div>
       <h1>Notes</h1>
       <ul>
-        <li>{notes[0].content}</li>
-        <li>{notes[1].content}</li>
-        <li>{notes[2].content}</li>
-        <li>{notes[3].content}</li>
-        <li>{ notes[4].content }</li>
+        {
+          notes.map(note => 
+            <li key={note.id}>{ note.content }</li>
+          )
+        }
       </ul>
     </div>
   )
