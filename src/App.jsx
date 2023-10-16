803c56d7295cab33d8eaeb9e4a899e8d87ca19d2
githubscientist <sathish.datascientist@gmail.com>
2023-10-16 19:32:32 +0530

    component lifecycle

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
-import React, { Component } from 'react';
-
-class Hello extends Component {
-  render() {
-
-    // console.log(this.props);
-    const { name } = this.props;
-
-    return (
-      <p>Hello { name }!</p>
-    )
-  }
-}
+import { Component } from "react";
 
 class App extends Component {
+
+  // componentDidMount: called after the component has been rendered to the DOM
+  componentDidMount() {
+    console.log('Component mounted');
+  }
+
+  // called immediately after the component updated in the DOM
+  // called on state or prop changes
+  componentDidUpdate() {
+    console.log('component state updated');
+  }
+
+  componentWillUnmount() {
+    // invoked just before the component is removed from the DOM
+    // releasing memory or resources
+    // detaching the events
+    // cancelling timers
+  }
+
   render() {
     return (
-      <div>
-        <Hello name='Sathish' />
-      </div>
+      <p>Hello World!</p>
     )
   }
 }
 
 export default App;
+
+// component lifecycle: the different states/stages a component takes from getting created and rendered in the DOM to getting removed from the DOM
