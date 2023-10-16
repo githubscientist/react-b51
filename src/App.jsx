20273dbe0e15bb379061809f6a5cc13b52307e36
githubscientist <sathish.datascientist@gmail.com>
2023-10-16 19:17:32 +0530

    creating multiple class components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
+class Hello extends Component {
+  render() {
+    return (
+      <p>Hello World!</p>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div>
-        <p>Hello World!</p>
+        <Hello />
       </div>
     )
   }
