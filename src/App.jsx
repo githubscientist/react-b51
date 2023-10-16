93213aca214651b5de0940b7789237b7ccc6faf5
githubscientist <sathish.datascientist@gmail.com>
2023-10-16 19:10:56 +0530

    functional component vs class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+// Function component
+// created as Javascript functions
+// it should return the jsx => HTML with Javascript embedded
+// function App() {
+//   return (
+//     <div>Hello World!</div>
+//   )
+// }
 
-function App() {
-  return (
-    <div>App</div>
-  )
+// export default App;
+
+// --------------------------------------------------------
+
+import { Component } from "react";
+
+// Class component
+class App extends Component {
+  render() {
+    return (
+      <div>Hello World!</div>
+    )
+  }
 }
 
 export default App;
