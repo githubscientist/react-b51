0784dcc6bb60208072558d83a17a0a32957b432c
githubscientist <sathish.datascientist@gmail.com>
2023-10-16 19:16:09 +0530

    creating a class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Hello({ name }) {
-  return (
-    <p>Hello { name }!</p>
-  )
-}
-
-function App() {
-  return (
-    <div>
-      <Hello name='Sathish' />
-    </div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hello World!</p>
+      </div>
+    )
+  }
 }
 
 export default App;
