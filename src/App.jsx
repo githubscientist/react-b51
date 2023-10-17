37bf089100264cf9692f8b169ab7c8498426b441
githubscientist <sathish.datascientist@gmail.com>
2023-10-17 21:09:30 +0530

    props drilling

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,34 @@
-import React, { useEffect, useState } from 'react';
+// props drilling: props drilling occurs when you need to pass down the data through multiple nested components to reach the ones that require the data.
+
+import React from 'react';
+
+function GrandChildComponent({ data }) {
+  console.log(`from grand child component: ${data}`);
+  return (
+    <div>
+      <h3>Grand Child Component</h3>
+    </div>
+  )
+}
+
+function ChildComponent({ data }) {
+  console.log(data);
+  return (
+    <div>
+      <h2>Child Component</h2>
+      <GrandChildComponent data={ data } />
+    </div>
+  )
+}
 
 function App() {
 
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    document.title = `Count: ${count}`;
-  }, [count]);
-
-  const handleChange = () => {
-    setCount(count + 1);
-  }
-
-  console.log(count);
+  const parentData = `Hello from Parent`;
 
   return (
     <div>
-      <h2>Document Title Change</h2>
-      <button onClick={handleChange}>Change Count</button>
+      <h2>Parent Component</h2>
+      <ChildComponent data={ parentData } />
     </div>
   )
 }
