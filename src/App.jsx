0fa6b22500cba2f5f2a4ebe7ac063d0e2f353a17
githubscientist <sathish.datascientist@gmail.com>
2023-10-17 21:00:18 +0530

    useEffect hook with dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,23 @@
-/*
-  useEffect Hook:
-    - useEffect hook is used in functional components in React to manage the side effects [data fetching, DOM manipulation, subscriptions].
-*/
-
 import React, { useEffect, useState } from 'react';
 
 function App() {
 
-  // create a state to store the data fetched from the API
-  const [posts, setPosts] = useState(null);
+  const [count, setCount] = useState(0);
 
-  const fetchPosts = async () => {
-    let response = await fetch(`https://jsonplaceholder.typicode.com/posts`)
-    let data = await response.json();
-    setPosts(data);
+  useEffect(() => {
+    document.title = `Count: ${count}`;
+  }, [count]);
+
+  const handleChange = () => {
+    setCount(count + 1);
   }
 
-  // data has to be fetched
-  // the following will run only once
-  useEffect(() => {
-    fetchPosts();
-  }, []);
+  console.log(count);
 
   return (
     <div>
-      <h2>API DATA</h2>
-      {
-        posts ? (
-          <ul>
-            {
-              posts.map(post => 
-                <li key={post.id}>{ post.title }</li>
-              )
-            }
-          </ul>
-        ) : (
-            <p>Fetching Data...</p>
-        )
-      }
+      <h2>Document Title Change</h2>
+      <button onClick={handleChange}>Change Count</button>
     </div>
   )
 }
