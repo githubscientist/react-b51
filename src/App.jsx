abced8322068d33dee802f6a3638f4164cb4b1bd
githubscientist <sathish.datascientist@gmail.com>
2023-10-17 19:45:04 +0530

    adding counter using a function of time

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import Note from './components/Note';
-import './styles/App.css';
+/*
+  Hooks: 
+    - any function that starts with "use" is called a hook
+    - they are special functions that are available only while React is rendering.
 
-function App({notes}) {
+    To create a counter, where the value is increased as a function of time or at the click of a button.
+*/
+
+import React, { useState } from 'react';
+
+function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  setTimeout(() => {
+    setCounter(counter + 1);
+  }, 1000);
+
+  console.log('rendering...', counter);
 
   return (
-    <div>
-      <h1>Notes</h1>
-      <ul id='list'>
-        {
-          notes.map(note => 
-            <Note key={ note.id } note={ note } />
-          )
-        }
-      </ul>
-
-      <form>
-        <input />
-        <button onClick={() => console.log('save button clicked')}>Save Note</button>
-      </form>
-    </div>
+    <div>App</div>
   )
 }
 
