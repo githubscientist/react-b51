37296802e301658ceacd2706e5718288dc416444
githubscientist <sathish.datascientist@gmail.com>
2023-10-19 20:17:36 +0530

    install boostrap + import bootstrap 4.6.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import './styles/App.css';
 
+// import bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+
 function App() {
   return (
     <div>
