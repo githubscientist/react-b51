cf27ba9ec82618a14678de898381c5040be455e5
githubscientist <sathish.datascientist@gmail.com>
2023-10-25 20:45:07 +0530

    useReducer simple example

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
-import React, { useReducer, useState } from 'react';
-import { initialState, reducer } from './reducers/countReducer';
+// useReducer: Its a hook that is built-in within the react library and it is used for managing more complex state logic in your components.
+import React, { useReducer } from 'react';
+
+// 1. define initialState
+const initialState = {
+  value: '',
+}
+
+// 2. define reducer
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case 'change': 
+      return { value: action.payload }
+    default:
+      return state;
+  }
+}
 
 function App() {
 
-  const [state, dispatch] = useReducer(reducer, initialState);
+  // 3. get the state + dispatcher
+  const [state, dispatch] = useReducer(inputReducer, initialState);
 
   return (
     <div>
-      <h3>Counter: { state.count }</h3>
-      <button onClick={() => dispatch({ type: 'incr' })}>Increment</button>
-      <button onClick={() => dispatch({ type: 'decr' })}>Decrement</button>
-      <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
+      <input 
+        type='text'
+        placeholder='type something...'
+        onChange={e => dispatch({ type: 'change', payload: e.target.value })}
+      />
 
-      <h4>Clicks History</h4>
-      <p>{  }</p>
+      <p>You typed: { state.value }</p>
     </div>
   )
 }
