50fcc32ac989a8642d39f00efe8d8288dd341109
githubscientist <sathish.datascientist@gmail.com>
2023-10-25 19:26:41 +0530

    body background color switcher

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,18 @@ import React, { useRef } from 'react';
 function App() {
 
   // get the reference of the button
-  const buttonRef = useRef(null);
+  const bodyRef = useRef(document.body);
 
   const changeColor = () => {
-    // console.log('change color button clicked!');
-    // console.log(buttonRef.current);
     const colors = ['red', 'blue', 'green', 'yellow', 'purple', 'pink', 'black', 'brown', 'orange', 'white'];
     const randomColor = colors[Math.floor(Math.random()*10)];
-    console.log(randomColor);
-    buttonRef.current.style.backgroundColor = randomColor;
+    bodyRef.current.style.backgroundColor = randomColor;
   }
 
   return (
     <div>
       <h3>Background Color Switcher</h3>
-      <button onClick={changeColor} ref={buttonRef}>Change Color</button>
+      <button onClick={changeColor}>Change Color</button>
     </div>
   )
 }
