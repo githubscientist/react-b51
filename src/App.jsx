16a8e13126726863cf695e650d7ed83048851f38
githubscientist <sathish.datascientist@gmail.com>
2023-10-25 19:24:45 +0530

    button background color switcher with useRef hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,23 @@
-// useRef hook: 
-/*
-
-  - It's a react's built in feature
-  - To create and manage references of DOM elements
-  - We can manipulate the element using the reference
-  without any unnecessary re-renders
-
-*/
-import React, { useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
 
 function App() {
 
-  // create a reference using useRef hook
-  const inputRef = useRef(null);
+  // get the reference of the button
+  const buttonRef = useRef(null);
 
-  useEffect(() => {
-    inputRef.current.focus();
-  }, []);
+  const changeColor = () => {
+    // console.log('change color button clicked!');
+    // console.log(buttonRef.current);
+    const colors = ['red', 'blue', 'green', 'yellow', 'purple', 'pink', 'black', 'brown', 'orange', 'white'];
+    const randomColor = colors[Math.floor(Math.random()*10)];
+    console.log(randomColor);
+    buttonRef.current.style.backgroundColor = randomColor;
+  }
 
   return (
     <div>
-      <input 
-        type='text'
-        placeholder='type a note...'
-        ref={inputRef}
-      />
+      <h3>Background Color Switcher</h3>
+      <button onClick={changeColor} ref={buttonRef}>Change Color</button>
     </div>
   )
 }
