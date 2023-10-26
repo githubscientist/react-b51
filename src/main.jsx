89d9833d118b3b013ac7372777d9646cdd3db830
githubscientist <sathish.datascientist@gmail.com>
2023-10-26 20:28:51 +0530

    db.json added + mock server created using json-server

--- src/main.jsx
+++ src/main.jsx
@@ -2,34 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
-// 1. define the data notes
-const notes = [
-    {
-        id: 1,
-        content: 'HTML is easy',
-        important: true
-    },
-    {
-        id: 2,
-        content: 'Browser can only execute Javascript',
-        important: false
-    },
-    {
-        id: 3,
-        content: 'GET and POST are the most important methods of HTTP protocol',
-        important: true
-    },
-    {
-        id: 4,
-        content: 'JSON server acts like a mock server without a real backend server to serve API endpoints',
-        important: false
-    },
-    {
-        id: 5,
-        content: 'CRUD with React with routers, useState, useReducers, useContext, useRef hooks axios',
-        important: true
-    }
-];
-
 // 2. pass the data notes as props to the App component
-ReactDOM.createRoot(document.getElementById('root')).render(<App notes={ notes } />);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
