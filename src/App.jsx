01573a7a4a37f2d8cb97a9dbb3a4cc66a26b4fb8
githubscientist <sathish.datascientist@gmail.com>
2023-10-26 19:11:46 +0530

    read the notes --CRUD

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,23 @@
 import React, { useState } from 'react';
-import './styles/App.css';
 
-function Button({ value, onClick }) {
-  return (
-    <button className='square' onClick={onClick}>{ value }</button>
-  )
-}
+// 3. read the notes list and render it here
 
-function App() {
+function App(props) {
 
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-
-  const handleClick = (i) => {
-
-    if (squares[i] || calculateWinner(squares)) {
-      return;
-    }
-
-    const nextSquares = squares.slice();
-
-    if (xIsNext) {
-      nextSquares[i] = 'X';
-    } else {
-      nextSquares[i] = 'O';
-    }
-
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
-  }
-
-  const calculateWinner = (squares) => {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-
-    for (let i = 0; i < lines.length; i++){
-      const [a, b, c] = lines[i];
-
-      if (squares[a] && squares[a] == squares[b] && squares[a] == squares[c]) {
-        return squares[a];
-      }
-    }
-
-    return null;
-  }
-
-  let winner = calculateWinner(squares);
-  let status;
-
-  if (winner) {
-    status = 'Winner: ' + winner;
-  } else {
-    status = 'Next Player: ' + (xIsNext ? 'X' : 'O');
-  }
+  // define a state to store the notes from props
+  const [notes, setNotes] = useState(props.notes);
 
   return (
     <div>
-      <div>
-        {status}
-      </div>
-      <div className='row'>
-        <Button value={squares[0]} onClick={ () => handleClick(0) } />
-        <Button value={ squares[1] } onClick={ () => handleClick(1) }/>
-        <Button value={ squares[2] } onClick={ () => handleClick(2) }/>
-      </div>
+      <h1>Notes</h1>
 
-      <div className='row'>
-        <Button value={ squares[3] } onClick={ () => handleClick(3) }/>
-        <Button value={ squares[4] } onClick={ () => handleClick(4) }/>
-        <Button value={ squares[5] } onClick={ () => handleClick(5) }/>
-      </div>
-
-      <div className='row'>
-        <Button value={ squares[6] } onClick={ () => handleClick(6) }/>
-        <Button value={ squares[7] } onClick={ () => handleClick(7) }/>
-        <Button value={ squares[8] } onClick={ () => handleClick(8) }/>
-      </div>
-
+      <ul>
+        {
+          notes.map(note => 
+            <li key={note.id}>{ note.content }</li>
+          )
+        }
+      </ul>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+// 1. define the data notes
+const notes = [
+    {
+        id: 1,
+        content: 'HTML is easy',
+        important: true
+    },
+    {
+        id: 2,
+        content: 'Browser can only execute Javascript',
+        important: false
+    },
+    {
+        id: 3,
+        content: 'GET and POST are the most important methods of HTTP protocol',
+        important: true
+    },
+    {
+        id: 4,
+        content: 'JSON server acts like a mock server without a real backend server to serve API endpoints',
+        important: false
+    },
+    {
+        id: 5,
+        content: 'CRUD with React with routers, useState, useReducers, useContext, useRef hooks axios',
+        important: true
+    }
+];
+
+// 2. pass the data notes as props to the App component
+ReactDOM.createRoot(document.getElementById('root')).render(<App notes={ notes } />);
