15ab633783dd6d5f367e4b4d771daeb98f0561ee
githubscientist <sathish.datascientist@gmail.com>
2023-11-01 20:38:25 +0530

    toggle importance added --manual

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,31 @@ import React from 'react';
 import { createStore } from 'redux';
 
 const noteReducer = (state = [], action) => {
-  if (action.type == 'NEW_NOTE') {
-    state.push(action.payload);
-    return state;
-  }
+  switch (action.type) {
+    case 'NEW_NOTE':
+      return state.concat(action.payload);
+    
+    case 'TOGGLE_IMPORTANCE':
+      // get the id from the action.payload
+      const id = action.payload.id;
 
-  return state;
+      // get the current state's object matching the id
+      const noteToChange = state.find(n => n.id === id);
+
+      // make the necessary changes
+      const changedNote = {
+        ...noteToChange,
+        important: !noteToChange.important,
+      }
+
+      // return the new object
+      return state.map(note => 
+          note.id !== id ? note : changedNote
+        )
+    
+    default:
+      return state;
+  }
 }
 
 const store = createStore(noteReducer);
@@ -27,6 +46,20 @@ store.dispatch({
     id: 2,
     content: "Browser can only execute Javascript",
     important: false
+  }
+})
+
+store.dispatch({
+  type: 'TOGGLE_IMPORTANCE',
+  payload: {
+    id: 2,
+  }
+})
+
+store.dispatch({
+  type: 'TOGGLE_IMPORTANCE',
+  payload: {
+    id: 1,
   }
 })
 
