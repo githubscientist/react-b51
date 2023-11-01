79cf20be118d99071def104ef0653d2bec815e01
githubscientist <sathish.datascientist@gmail.com>
2023-11-01 18:56:29 +0530

    initial cleaned project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
 import React from 'react';
-import Header from './components/Header';
-import LetterGrid from './components/LetterGrid';
-import Keyboard from './components/Keyboard';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <LetterGrid />
-      <Keyboard />
-    </div>
+    <div>App</div>
   )
 }
 
