450b7718b7f193f8950fc48f403d5ec9a954de6c
githubscientist <sathish.datascientist@gmail.com>
2023-11-01 19:24:51 +0530

    simple redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { createStore } from 'redux';
+
+/*
+    action = {
+        type: 'INCR'
+    }
+*/
+const counterReducer = (state = 0, action) => {
+    console.log(action);
+    switch (action.type) {
+        case 'INCR':
+            return state + 1;
+        case 'DECR':
+            return state - 1;
+        default:
+            return state;
+    }
+}
+
+// create a store
+const store = createStore(counterReducer);
+
+// get the current store state
+console.log(store.getState());
+
+// increment the counter
+store.dispatch({
+    type: 'INCR'
+})
+
+console.log(store.getState());
+
+// increment the counter
+store.dispatch({
+    type: 'INCR'
+})
+
+console.log(store.getState());
+
+// increment the counter
+store.dispatch({
+    type: 'DECR'
+})
+
+console.log(store.getState());
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
