c1d7ffab749bbb80629dd3d965695943e5fa57e6
githubscientist <sathish.datascientist@gmail.com>
2023-11-01 20:02:09 +0530

    moving App.jsx to its own file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import { useDispatch, useSelector } from "react-redux";
 
-function App() {
-  return (
-    <div>App</div>
-  )
+const App = () => {
+
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state);
+
+    return (
+        <div>
+            <div>
+                Count: {counter}
+            </div>
+
+            <button
+                onClick={() => dispatch({type: 'INCR'})}
+            >plus</button>
+            <button
+                onClick={() => dispatch({ type: 'DECR' })}
+            >minus</button>
+            <button
+                onClick={() => dispatch({ type: 'ZERO' })}
+            >zero</button>
+        </div>
+    )
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App.jsx';
+import App from './App.jsx';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-/*
-    action = {
-        type: 'INCR'
-    }
-*/
 const counterReducer = (state = 0, action) => {
     switch (action.type) {
         case 'INCR':
@@ -24,32 +20,8 @@ const counterReducer = (state = 0, action) => {
 // create a store
 const store = createStore(counterReducer);
 
-const App = () => {
-    return (
-        <div>
-            <div>
-                Count: {store.getState()}
-            </div>
-
-            <button
-                onClick={() => store.dispatch({type: 'INCR'})}
-            >plus</button>
-            <button
-                onClick={() => store.dispatch({ type: 'DECR' })}
-            >minus</button>
-            <button
-                onClick={() => store.dispatch({ type: 'ZERO' })}
-            >zero</button>
-        </div>
-    )
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-const renderApp = () => {
-    root.render(<App />);
-}
-
-renderApp();
-
-store.subscribe(renderApp);
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
