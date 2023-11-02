7474b16d411c703b1c506672db59d34ca15b0945
githubscientist <sathish.datascientist@gmail.com>
2023-11-02 20:31:12 +0530

    like & unlike button counters

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,23 @@
-import axios from 'axios';
-import React from 'react';
-
-/*
-  axios: 
-    axios is a library that helps us to make http requests.
-    It returns a promise.
-    axios will internally use fetch api to make http requests.
-  
-  we can handle axios calls in two ways:
-    1. using async await -> asynchronous (but internally it is synchronous because of await keyword)
-    2. using then and catch -> synchronous
-  
-  synchronous: the statements are executed line by line.
-  asynchronous: the statements are not executed line by line or in the background one by one.
-
-  to install: npm install axios
-*/
+import { faThumbsDown, faThumbsUp } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
 
 function App() {
 
-  const fetchPosts = () => {
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-      .then((response) => {
-        console.log(response.data);
-        ;
-      })
-      .catch((error) => {
-        console.log('error fetching posts...', error);
-      })
-  }
+  const [like, setLike] = useState(0);
+  const [dislike, setDislike] = useState(0);
 
   return (
     <div>
-      <button onClick={fetchPosts}>Fetch Posts</button>
+      <div>
+        <button onClick={() => setLike(like + 1)}>{<FontAwesomeIcon icon={faThumbsUp} size='2xl'/>}</button>
+        &nbsp;&nbsp;&nbsp;<span>{ like }</span>
+      </div>
+      <p></p>
+      <div>
+        <button onClick={() => setDislike(dislike + 1)}>{<FontAwesomeIcon icon={faThumbsDown} size='2xl'/>}</button>
+        &nbsp;&nbsp;&nbsp;<span>{dislike}</span>
+      </div>
     </div>
   )
 }
